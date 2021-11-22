b36e18a69777d1537ab585afd08403d96901863a
Balazshazialex <balazshazia@gmail.com>
2021-11-22 10:26:00 +0200

    Initial grammar thingy

---
+++ grammar.py
@@ -0,0 +1,28 @@
+class Grammar:
+    def __init__(self, filename):
+        self._non_terminals=[]
+        self._terminals=[]
+        self._starting_symbol=None
+        self._productions=[]
+        self.read_from_file(filename)
+
+    def read_from_file(self, filename):
+            with open(filename,'r') as file:
+                self._non_terminals=file.readline().strip().split(' ')
+                self._terminals=file.readline().strip().split(' ')
+                self._starting_symbol=file.readline().strip()
+                for line in file:
+                    production = line.strip().split(' ')
+                    ok=0
+                    key = production[0].strip()
+                    values = list(production[2].strip().split('|'))
+                    for value in values:
+                        self._productions.append(
+                            (key, list(value))
+                        )
+            file.close()
+
+
+
+g=Grammar('grammar1.txt')
+print(g._productions)
